d4c2d22ca9d5d2e2fa45e4b7afa0b46a8ad246bd
fandoghi <55033503+fandoghi@users.noreply.github.com>
2019-10-27 18:14:28 +0330

    added get move
    change tuples to list
    
    Co-Authored-By: el-s <el-s@users.noreply.github.com>

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,1 +1,8 @@
-list_tic_tac_toe = [(0,0,0),(0,0,0),(0,0,0)]
+list_tic_tac_toe = [[0,0,0],[0,0,0],[0,0,0]]
+
+def get_move():
+    move = input()
+    x = move[0]
+    y = move[1]
+    return x,y
+print(get_move())
